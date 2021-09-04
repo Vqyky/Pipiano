97af932f2195601df6ce6e515db3c45d6644bed9
JossB <j.bucquelier@gmail.com>
2021-09-04 12:50:33 +0200

    Libs

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+# IMPORTS
+import mido
+#
+# from mido import Message
+#
+# msg = Message('note_on', note=60)
+# print(msg)
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Open input and receive message
+maininput = mido.open_input()
+msg = maininput.receive()
 
+print(msg)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
